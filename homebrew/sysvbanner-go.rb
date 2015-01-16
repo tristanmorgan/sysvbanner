1bd3f9a61729a4129e5ff7cff572ffd83c9e065a
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-01-16 23:17:26 +0900

    Fix hash

--- homebrew/sysvbanner-go.rb
+++ homebrew/sysvbanner-go.rb
@@ -3,7 +3,7 @@ require 'formula'
 class SysvbannerGo < Formula
   homepage 'https://github.com/winebarrel/sysvbanner-go'
   url 'https://github.com/winebarrel/sysvbanner-go/releases/download/0.1.1/sysvbanner-go-0.1.1-darwin-amd64.tar.gz'
-  sha1 '0c4ff71a441d115555b67e9076c01f006c8ad6f3'
+  sha1 'a119a21bc08f11745dfad81e38a53a1d27914006'
   version '0.1.1'
   head 'https://github.com/winebarrel/sysvbanner-go.git', :branch => 'master'
 

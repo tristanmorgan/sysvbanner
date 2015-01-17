0c2fdada4b3035c24d17b93d720f7155dea29c7d
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-01-17 16:17:32 +0900

    Fix formula

--- homebrew/sysvbanner-go.rb
+++ homebrew/sysvbanner-go.rb
@@ -1,10 +1,12 @@
 require 'formula'
 
+SYSVBANNER_GO_VERSION = '0.1.2'
+
 class SysvbannerGo < Formula
   homepage 'https://github.com/winebarrel/sysvbanner-go'
-  url 'https://github.com/winebarrel/sysvbanner-go/releases/download/0.1.1/sysvbanner-go-0.1.1-darwin-amd64.tar.gz'
-  sha1 'a119a21bc08f11745dfad81e38a53a1d27914006'
-  version '0.1.1'
+  url "https://github.com/winebarrel/sysvbanner-go/releases/download/#{SYSVBANNER_GO_VERSION}/sysvbanner-go-#{SYSVBANNER_GO_VERSION}-darwin-amd64.tar.gz"
+  sha1 '2ce97081f053d603813d33be509af0813e0a2f97'
+  version SYSVBANNER_GO_VERSION
   head 'https://github.com/winebarrel/sysvbanner-go.git', :branch => 'master'
 
   def install

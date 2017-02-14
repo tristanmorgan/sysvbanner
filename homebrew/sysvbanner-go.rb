6c254c23d3a12c95ab22071861017af77c4e5dfa
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2017-02-14 14:41:33 +0900

    Update sysvbanner-go.rb

--- homebrew/sysvbanner-go.rb
+++ homebrew/sysvbanner-go.rb
@@ -5,7 +5,7 @@ SYSVBANNER_GO_VERSION = '0.1.2'
 class SysvbannerGo < Formula
   homepage 'https://github.com/winebarrel/sysvbanner-go'
   url "https://github.com/winebarrel/sysvbanner-go/releases/download/#{SYSVBANNER_GO_VERSION}/sysvbanner-go-#{SYSVBANNER_GO_VERSION}-darwin-amd64.tar.gz"
-  sha256 '65e51fcbe4319b13248c14a1445176ccb194bb7323ddb8133f9266ce9255fa14'
+  sha256 '1192c8cb86b11dc46c7c60ff70c01cf75f7648af5ce431753f2ecb2bc3aef655'
   version SYSVBANNER_GO_VERSION
   head 'https://github.com/winebarrel/sysvbanner-go.git', :branch => 'master'
 
